085e8939095eed7d105100254777706ba0a79d08
Erik Morbach <morbacherik@gmail.com>
2023-07-30 21:39:08 -0300

    ajustes de primalidade

--- math/brent.cpp
+++ math/brent.cpp
@@ -55,21 +55,43 @@ ll f(ll x, ll c, ll mod){
 	return (mul(x,x,mod) + c)%mod;
 }
 
-ll rho(ll n) {
+ll brent(ll n){
 	if(isPrime(n)) return n;
 	for(int w: {2,3,5,7,11,13,17,19})
 		if(n%w==0) return w;
-	ll x0 = (rnd()%(n-2)) + 2;
-	ll c = (rnd()%(n-1)) + 1;
-    ll x = x0;
-    ll y = x0;
-    ll g = 1;
-    while (g == 1) {
-        x = f(x, c, n);
-        y = f(y, c, n);
-        y = f(y, c, n);
-        g = __gcd(abs(x - y), n);
-		if(g == n) return rho(n);
-    }
-    return g;
+	ll x0 = (ra()%(n-1)) + 1;
+	ll c = (ra()%(n-1))+1;
+	ll turt = x0;
+	ll rabit = f(turt, c, n);
+	ll g = 1;
+
+	ll lastRabit = rabit;
+	int l = 1;
+	int m = 128;
+	while(g==1){
+		turt = rabit;
+		for(int i=0;i<l;i++)
+			rabit = f(rabit, c, n);
+
+		ll q = 1;
+		for(int k=0;k<l;k+=m){
+			lastRabit = rabit;
+			int lim = min(m, l-k);
+			for(int i=0;i<lim; i++){
+				rabit = f(rabit, c, n);
+				q = mul(q, abs(rabit - turt), n);
+			}
+			g = __gcd(q, n);
+			if(g!=1) break;
+		}
+		l *= 2;
+	}
+	if(g == n){
+		g = 1;
+		while(g == 1){
+			lastRabit = f(lastRabit, c, n);
+			g = __gcd(abs(lastRabit - turt), n);
+		}
+	}
+	return g;
 }
--- math/fermatPrimeTest.cpp
+++ math/fermatPrimeTest.cpp
@@ -1,6 +1,24 @@
+ll rnd(){
+	static mt19937_64 rn(chrono::system_clock::now().time_since_epoch().count());
+	ll r = rn();
+	if(r<0) r = -r;
+	return r % (1LL<<62);
+}
+// Preferir essa implementacao
 ll mul(ll a, ll b, ll m){
 	return (__int128(a) * b)%m;
 }
+/*
+ll mul(ll a, ll b, ll m){
+	ll result = 0;
+	while(b){
+		if(b&1) result = (result + a)%m;
+		a = (a+a)%m;
+		b>>=1;
+	}
+	return result;
+}*/
+
 ll binPow(ll base, ll ex, ll mod){
 	if(ex==0) return 1;
 	if(ex==1) return base;
@@ -14,7 +32,7 @@ bool isPrime(ll p){
 	if(p < 4) return p==2 || p == 3;
 	int it = 10;
 	while(it--){
-		ll a = ((rand())%(p-3)) + 2;
+		ll a = ((rnd())%(p-3)) + 2;
 		if(binPow(a,p-1, p) != 1) return false;
 	}
 	return true;
